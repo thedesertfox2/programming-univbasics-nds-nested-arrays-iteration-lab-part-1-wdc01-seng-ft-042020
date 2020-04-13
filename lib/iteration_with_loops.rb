02a8f967896a88f2138c935580813b2ea08e9726
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-13 14:45:22 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,5 +7,15 @@ def find_even_values(src)
     [4,5,6],
     [7,8,9]
     ]
-find_even_values(array_of_arrays)
+  row_index = 0
+  while row_index < src.count do
+    element_index = 0
+    while element_index < src[row_index].count do
+      if src[row_index][element_index].even?
+        p src[row_index][element_index]
+      end
+      element_index += 1
+    end
+    row_index += 1
+  end
 end
